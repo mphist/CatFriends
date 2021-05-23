d6ed6d833ef8920674c42dbd4090524e915c6c70
Kevin Baik <mphist@gmail.com>
2021-05-23 16:37:26 -0400

    Prisma setup

---
+++ packages/server/src/index.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '.prisma/client'
+import Server from './Server'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const prisma = new PrismaClient()
+
+prisma
+  .$connect()
+  .then(async () => {
+    const server = new Server()
+    await server.start()
+  })
+  .catch((e) => console.log(e))
